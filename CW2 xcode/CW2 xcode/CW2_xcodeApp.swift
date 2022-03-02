27092b6249c2c0a3585ff9c9371190973ccfebc4
Abdullah-alnoumas <70814254+Abdullah-alnoumas@users.noreply.github.com>
2022-03-02 20:21:00 +0300

    I changed the print statement

---
+++ CW2 xcode/CW2 xcode/CW2_xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CW2_xcodeApp.swift
+//  CW2 xcode
+//
+//  Created by Abdullah Alnoumas on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CW2_xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CW2 xcode/CW2 xcode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CW2 xcode
+//
+//  Created by Abdullah Alnoumas on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hi my name is Abdullah Al Noumas a IOS Development student !!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
